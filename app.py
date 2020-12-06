762bdb51e44607794267731b18e416e6f8e7fc43
Siyavush3 <69103812+Siyavush3@users.noreply.github.com>
2020-12-06 13:59:44 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -18,13 +18,50 @@ def graph():
 
 
 
+
+
+
 mpl.rcParams['figure.figsize'] = (8, 6)
 mpl.rcParams['axes.grid'] = False
 
-df = pd.read_csv('1.csv')
+
+df = pd.read_csv(1.csv)
+def univariate_data(dataset, start_index, end_index, history_size, target_size):
+  data = []
+  labels = []
+
+  start_index = start_index + history_size
+  if end_index is None:
+    end_index = len(dataset) - target_size
+
+  for i in range(start_index, end_index):
+    indices = range(i-history_size, i)
+    # Reshape data from (history_size,) to (history_size, 1)
+    data.append(np.reshape(dataset[indices], (history_size, 1)))
+    labels.append(dataset[i+target_size])
+  return np.array(data), np.array(labels)
 
 
+TRAIN_SPLIT = 300000
+tf.random.set_seed(13)
 
+uni_data = df['T (degC)']
+uni_data.index = df['Date Time']
+print(uni_data)
+uni_data.plot(subplots=True)
+uni_data = uni_data.values
+uni_train_mean = uni_data[:TRAIN_SPLIT].mean()
+uni_train_std = uni_data[:TRAIN_SPLIT].std()
+uni_data = (uni_data-uni_train_mean)/uni_train_std
+univariate_past_history = 20
+univariate_future_target = 0
+
+x_train_uni, y_train_uni = univariate_data(uni_data, 0, TRAIN_SPLIT,
+                                           univariate_past_history,
+                                           univariate_future_target)
+x_val_uni, y_val_uni = univariate_data(uni_data, TRAIN_SPLIT, None,
+                                       univariate_past_history,
+                                       univariate_future_target)
 
 def family_palov_index(meat,carrot,rise,oil,onion):
     index=0
